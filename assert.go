cf259b37ba382eb490c42738f8d666300d49db5a
Artem Yadelskyi <mymmrac@gmail.com>
2023-11-03 20:41:57 +0200

    Improved assert

--- assert.go
+++ assert.go
@@ -3,11 +3,17 @@ package x
 import (
 	"fmt"
 	"os"
+	"runtime"
 )
 
 func Assert(ok bool, args ...any) {
 	if !ok {
-		fmt.Fprintf(os.Stderr, "ASSERT: %s\n", fmt.Sprint(args...))
+		_, file, line, _ := runtime.Caller(1)
+		if len(args) == 0 {
+			fmt.Fprintf(os.Stderr, "ASSERT: %s:%d\n", file, line)
+		} else {
+			fmt.Fprintf(os.Stderr, "ASSERT: %s:%d: %s\n", file, line, fmt.Sprint(args...))
+		}
 		os.Exit(1)
 	}
 }
