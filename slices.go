323d7269004de43ce8eb1fee224632f0d320477b
Artem Yadelskyi <mymmrac@gmail.com>
2023-12-07 10:47:44 +0200

    Updated slices

--- slices.go
+++ slices.go
@@ -4,16 +4,16 @@ import (
 	"cmp"
 )
 
-func Convert[S1 ~[]E1, S2 ~[]E2, E1, E2 any](x S1, conv func(a E1) E2) S2 {
-	y := make(S2, len(x))
+func Convert[T1, T2 any](x []T1, conv func(item T1) T2) []T2 {
+	y := make([]T2, len(x))
 	for i := 0; i < len(x); i++ {
 		y[i] = conv(x[i])
 	}
 	return y
 }
 
-func Sum[S ~[]E, E cmp.Ordered](x S) E {
-	var s E
+func Sum[T cmp.Ordered](x []T) T {
+	var s T
 	for i := 0; i < len(x); i++ {
 		s += x[i]
 	}
